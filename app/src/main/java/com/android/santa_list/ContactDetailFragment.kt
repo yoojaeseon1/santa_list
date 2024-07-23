3f80e80344b8dc5abab685912b8189bdcbc92fc9
hyehyunj <developh620@gmail.com>
2024-07-23 20:51:21 +0900

    수정: 디테일페이지 알림, 선물 보완

--- app/src/main/java/com/android/santa_list/ContactDetailFragment.kt
+++ app/src/main/java/com/android/santa_list/ContactDetailFragment.kt
@@ -2,15 +2,23 @@ package com.android.santa_list
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
+import android.provider.Settings
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity.NOTIFICATION_SERVICE
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
 import com.android.santa_list.databinding.FragmentContactDetailBinding
 
 // TODO: Rename parameter arguments, choose names that match
@@ -51,15 +59,12 @@ class ContactDetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
-
-
-
-//        알림버튼 함수-------공사중
-        _binding?.detailBtnMessage?.setOnClickListener {
-
-        Log.d(TAG,"클릭")
-            val manager = requireContext().getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+//        알림버튼-------공사중
+        _binding?.detailIvAlert?.setOnClickListener {
+//            val intent = Intent(this.requireContext(), ContactDetailFragment::class.java)
+//            val pendingIntent = PendingIntent.getActivity(this.requireContext(), 0, intent, 0)
+            val manager =
+                requireContext().getSystemService(NOTIFICATION_SERVICE) as NotificationManager
             val builder: NotificationCompat.Builder
             //버전체크
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -81,17 +86,52 @@ class ContactDetailFragment : Fragment() {
             builder.run {
                 setSmallIcon(R.drawable.ic_alert_on)
                 setWhen(System.currentTimeMillis())
-                setContentTitle(getString(R.string.christmas))
+                setContentTitle(getString(R.string.christmas_eve))
                 setContentText(getString(R.string.christmas))
+//                setContentIntent(pendingIntent)
+                setAutoCancel(true)
+
             }
             manager.notify(1, builder.build())
         }
 
+       if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+           if (!NotificationManagerCompat.from(this.requireContext()).areNotificationsEnabled()) {
+               val intent = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS).apply {
+                   putExtra(Settings.EXTRA_APP_PACKAGE, `package`)
+               }
+               startActivity(intent)
+           }
+       }
+        when {
 
+        }
 
+        //선물하기버튼 : 클릭 시 다이얼로그 응답에 따라 카카오톡, 쿠팡으로 이동
+        _binding?.detailIvGift?.setOnClickListener {
+            val builder = AlertDialog.Builder(this.requireContext())
+            builder.setTitle(getString(R.string.gift))
+            builder.setMessage(getString(R.string.gift_shop))
+            builder.setIcon(R.drawable.ic_gift_grey)
 
+            val btnListener = DialogInterface.OnClickListener { dialog, which ->
+                when (which) {
+                    DialogInterface.BUTTON_POSITIVE -> {
+                        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://gift.kakao.com"))
+                        startActivity(intent)
+                    }
 
-
+                    DialogInterface.BUTTON_NEGATIVE -> {
+                        val intent =
+                            Intent(Intent.ACTION_VIEW, Uri.parse("https://www.coupang.com"))
+                        startActivity(intent)
+                    }
+                }
+            }
+            builder.setPositiveButton(getString(R.string.gift_shop_kakao), btnListener)
+            builder.setNegativeButton(getString(R.string.gift_shop_coupang), btnListener)
+            builder.show()
+        }
 
     }
 
@@ -114,8 +154,6 @@ class ContactDetailFragment : Fragment() {
                 }
             }
     }
-
-
 
 
 }
