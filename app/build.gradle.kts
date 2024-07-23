5e0b1fd6d69b0ac0acfa71d9640ab85b75a6059f
GGo-ging <inki0126@naver.com>
2024-07-23 15:38:42 +0900

    추가 : RecyclerView 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.android.support:recyclerview-v7:28.0.0")
 }
---
+++ app/src/main/java/com/android/santa_list/MainRecyclerViewAdapter.kt
@@ -0,0 +1,19 @@
+package com.android.santa_list
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import com.android.santa_list.dataClass.MyContact
+import com.android.santa_list.databinding.FragmentContactListBinding
+
+//class MainRecyclerViewAdapter (private val contact :MutableList<MyContact>) : RecyclerView.Adapter<MainRecyclerViewAdapter.Holder>(){
+//
+//
+//
+//    inner class Holder(val binding: FragmentContactListBinding) :
+//            RecyclerView.ViewHolder(binding.root) {
+//                val
+//            }
+//
+//
+//
+//}
