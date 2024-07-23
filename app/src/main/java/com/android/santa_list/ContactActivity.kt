6713e4de127d58e737b43cdbbf2377f4a03f4f80
kkevi <bora_kim980@naver.com>
2024-07-23 21:45:36 +0900

    추가 : Toolbar에서 action icon 클릭시 fragment 별로 다르게 동작하도록 변경

--- app/src/main/java/com/android/santa_list/ContactActivity.kt
+++ app/src/main/java/com/android/santa_list/ContactActivity.kt
@@ -11,6 +11,7 @@ import androidx.core.app.NotificationCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.commit
 import com.android.santa_list.dataClass.Dummy
 import com.android.santa_list.databinding.ActivityContactBinding
@@ -18,6 +19,7 @@ import com.google.android.material.tabs.TabLayout
 
 class ContactActivity : AppCompatActivity(), TabLayout.OnTabSelectedListener {
     private val binding: ActivityContactBinding by lazy { ActivityContactBinding.inflate(layoutInflater) }
+    private val fragmentManager: FragmentManager = supportFragmentManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,20 +31,41 @@ class ContactActivity : AppCompatActivity(), TabLayout.OnTabSelectedListener {
             insets
         }
 
+        // binding 관련된 코드들 추가할 때 여기서
         binding.run{
             tabLayout.tabLayout.addOnTabSelectedListener(this@ContactActivity) // 다형성을 추가한 것
+
+            // 툴바의 action icon 클릭할 때 동작 하는 부분
+            toolBar.action.setOnClickListener {
+                val currentFragment: Fragment? = fragmentManager.findFragmentById(R.id.frame_layout)
+                if(currentFragment != null){
+                    when(currentFragment){
+                        is ContactListFragment -> {
+                            Log.d("⏰ action Click", "메인 리스트입니다.")
+                        }
+                        is MyPageFragment -> {
+                            Log.d("⏰ action Click", "마이 페이지입니다.")
+                        }
+                        else -> {
+                            Log.d("⏰ action Click", "다른 화면입니다.")
+                        }
+                    }
+                }
+            }
         }
-        //임시개발용
+
+        // 임시 개발용
         setFragment(ContactDetailFragment())
     }
 
     private fun setFragment(fragment: Fragment) {
-        supportFragmentManager.commit {
+        fragmentManager.commit {
             replace(R.id.frame_layout, fragment)
             setReorderingAllowed(true)
             addToBackStack("")
         }
     }
+
     override fun onTabSelected(tab: TabLayout.Tab?) {
         when(tab!!.position){
             // 0번째 탭 눌렀을 때
